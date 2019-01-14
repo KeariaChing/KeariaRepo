4fb22e61b34db3a289748604ca0f2f57712334b0
Kearia <keariaouo@gmail.com>
2019-01-14 17:26:21 +0800

    coomit homework

---
+++ Kearia_Week1_assignment2.playground/Contents.swift
@@ -0,0 +1,60 @@
+//Assignment 2
+
+//1
+var Pi: Double = 3.141596
+
+//2
+let x: Int = 40
+let y: Int = 90
+let average = (x+y)/2
+
+//3-1
+let z: Double = 40.0
+let t: Double = 90.0
+let finalAverage = (z+t)/2
+print(finalAverage)
+
+//3-2
+var division = 10/3
+var anotherDivision = 10.0/3.0
+/*
+ 10/3 為整數的運算故不會出現小數點，而10.0/3.0 swift判斷為Double其運算值會出現小數點後之數值。
+*/
+
+//4
+var flag: Bool = true
+var inpuString: String = "Hello world."
+let bitsInBite: Int = 8
+let averageScore: Float = 86.8
+
+//5
+var salary = 22000
+salary += 28000
+salary
+
+//6
+//The equality operator in swift is '=='.
+
+//7
+let remain = 10%3
+
+//8
+//let是常數，其值一旦設定就不能改變；而var是變數，變數的值可以更改。
+
+
+//9
+/*
+ Use camel casing.
+ No whitespace.
+ Don't start with numbers or special characters.
+*/
+
+//10
+/*
+Swift是一種安全的語言，會自動判別撰寫出來的值的的類型，可以幫助我們確保它的正確性。
+*/
+
+//11
+/*
+ phoneNumer已申告為Int，不可更改為String。
+*/
